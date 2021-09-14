3a1a2d9704e8a2befa73ae9cd84738f50aeb30b4
justinrk1992 <justinrk92@yahoo.com>
2021-09-14 14:41:43 -0500

    commit tree 634537f66118da84291ac0406331be266f6f5f91

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,89 @@
 # your code goes here
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(amount)
+        @happiness = normalize(amount)
+    end
+
+    def hygiene=(amount)
+        if amount > 10
+            @hygiene = 10
+        elsif amount < 0
+            @hygiene = 0
+        else
+            @hygiene = amount
+        end
+    end
+
+    def normalize(number)
+        if number > 10
+            return 10
+        elsif number < 0
+            return 0
+        end
+            return number
+    end
+
+    def happy?
+        if @happiness > 7
+            true
+        else 
+            false
+        end
+    end
+
+    def clean?
+        if @hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def take_bath
+        self.hygiene += 4
+        '♪ Rub-a-dub just relaxing in the tub ♫'
+    end
+
+    def get_paid(salary)
+        @bank_account += salary
+        return "all about the benjamins"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            person.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            person.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+
+
+end
